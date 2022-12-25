3f5a39d0af2cd8462125da84bed2582b3ad55f2e
ozlemnurazlioglu <ozlemnurnazlioglu2002@gmail.com>
2022-12-26 01:12:00 +0300

    Update player.java
    I'm going to try a different way as I misunderstood here

--- player.java
+++ player.java
@@ -1,8 +1,8 @@
-public class Player {
+public class player {
     private myproject[] hand;
     private int score;
 
-    public Player() {
+    public player() {
         hand = new myproject[4];
         score = 0;
     }
@@ -11,24 +11,7 @@ public class Player {
         hand[index] = card;
     }
 
-    public int calculateScore() {
-        for (myproject card : hand) {
-            if (card.getRank().equals("Ace")) {
-                score += 1;
-            } else if (card.getRank().equals("2")) {
-                score += 2;
-            } else if (card.getRank().equals("Tile 10")) {
-                score += 3;
-            } else if (card.getRank().equals("Jack")) {
-                score += 1;
-            } else if (card.getRank().equals("Queen")) {
-                score += 2;
-            } else if (card.getRank().equals("King")) {
-                score += 3;
-            }
-        }
-        return score;
-    }
+    
 
     public void printHand() {
         for (myproject card : hand) {
