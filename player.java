495192097f4b0315ddc2fc821ba60f5e04fac4ae
ozlemnurazlioglu <ozlemnurnazlioglu2002@gmail.com>
2022-12-26 13:54:25 +0300

    Update player.java

--- player.java
+++ player.java
@@ -1,8 +1,8 @@
-public class player {
+public class Player {
     private myproject[] hand;
     private int score;
 
-    public player() {
+    public  void player() {
         hand = new myproject[4];
         score = 0;
     }
@@ -11,11 +11,32 @@ public class player {
         hand[index] = card;
     }
 
-    
+    public int calculateScore() {
+        for (myproject card : hand) {
+            if (card.getRank().equals("Ace")) {
+                score += 1;
+            } else if (card.getRank().equals("2")) {
+                score += 2;
+            } else if (card.getRank().equals("Tile 10")) {
+                score += 3;
+            } else if (card.getRank().equals("Jack")) {
+                score += 1;
+            } else if (card.getRank().equals("Queen")) {
+                score += 2;
+            } else if (card.getRank().equals("King")) {
+                score += 3;
+            }
+        }
+        return score;
+    }
 
     public void printHand() {
         for (myproject card : hand) {
             System.out.println(card.toString());
         }
     }
+
+    public void addCard(Player.myproject dealCard, int i) {
+    }
 }
+
